471fd9124170dc52c99b54675b26a1fea64055ed
Devesh Shukla <deveshshukla1603@gmail.com>
2023-11-14 00:09:39 +0530

    Nothing But only test

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 const head = <h1>This Is Head JSX</h1>;
 
-const headComp = () => (
-    <h2>This head component</h2>
-);
-
+const HeadComp = () => {
+    return (
+        <>
+        {head}
+        <h1>I am in head Component</h1>
+        </>
+    );
+};
 const Root = ReactDOM.createRoot(document.getElementById("root"));
-Root.render(headComp);
+Root.render(<HeadComp/>);
